e89b4b8cdb4a1019020989a35a52603a62300768
Mingxin Li <kate.li.0924@gmail.com>
2019-04-05 12:37:03 -0700

    added iterative maxi function

--- lab1.py
+++ lab1.py
@@ -2,7 +2,16 @@
 def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+   maxi = 0
+   if len(int_list) != 0:
+      for i in int_list:
+         if i is None:
+            raise ValueError
+         if maxi < i:
+            maxi = i
+      return maxi
+   else:
+      return None
 
 def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
