6df7eb2f3934eef4408874d62f7fd1bc8bdad467
Mingxin Li <kate.li.0924@gmail.com>
2019-04-05 17:29:42 -0700

    added reverse_rec using recursion

--- lab1.py
+++ lab1.py
@@ -3,10 +3,10 @@ def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
    maxi = 0
-   if len(int_list) != 0:
+   if int_list is None:
+      raise ValueError
+   elif len(int_list) != 0:
       for i in int_list:
-         if i is None:
-            raise ValueError
          if maxi < i:
             maxi = i
       return maxi
@@ -16,7 +16,17 @@ def max_list_iter(int_list):  # must use iteration not recursion
 def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
-   pass
+   if int_list == None:
+       raise ValueError
+   elif len(int_list) == 0:
+       return []
+   elif len(int_list) == 1:
+       n = [int_list[0]]
+       return n
+   else:
+       return reverse_rec(int_list[1:len(int_list)]) + [int_list[0]]
+
+
 
 def bin_search(target, low, high, int_list):  # must use recursion
    """searches for target in int_list[low..high] and returns index if found
